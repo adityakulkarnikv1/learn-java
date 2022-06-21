f11d4df7321aed09c6685ce2e5bf00da9af09200
adityakulkarnikv1 <adityakulkarnikv1@gmail.com>
2022-06-21 10:17:18 +0530

    Final final test commit

--- src/com/aditya/Test.java
+++ src/com/aditya/Test.java
@@ -1,9 +1,19 @@
 package com.aditya;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		
+		List<String> strings = new ArrayList<>();
+		
+		strings.add(null);
+		strings.add("null");
+		
+		System.out.println(strings);
 	}
 
 }
