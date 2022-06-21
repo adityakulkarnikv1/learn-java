ceaaa7c7d11ada3c85005fd1f93ff43b19a9262c
adityakulkarnikv1 <adityakulkarnikv1@gmail.com>
2022-06-21 22:15:24 +0530

    Simple line sorting using Collections framework

--- src/com/aditya/Test.java
+++ src/com/aditya/Test.java
@@ -1,19 +1,50 @@
 package com.aditya;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
 
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
 		
-		List<String> strings = new ArrayList<>();
+		File fileName = new File("file.txt");
 		
-		strings.add(null);
-		strings.add("null");
+		ArrayList<String> lines = new ArrayList<>();
 		
-		System.out.println(strings);
+		try {
+			FileReader fileReader = new FileReader(fileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			String line;
+			
+			while((line = bufferedReader.readLine()) != null) {
+				lines.add(line);
+			}
+			
+			long start = System.currentTimeMillis();
+			
+			Collections.sort(lines);
+			
+			long end = System.currentTimeMillis();
+			
+			
+			
+			System.out.println("Time taken: " + String.format("%.3f", (float)(end - start) / 1000));
+			
+//			for(String l : lines) {
+//				System.out.println(l);
+//			}
+			
+			bufferedReader.close();
+			fileReader.close();
+			
+		} catch(IOException ioException) {
+			ioException.printStackTrace();
+		}
 	}
 
 }
